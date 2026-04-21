732edebfa049705d395b883678ceefd4e0954eaa
Humberto Arana <humaranah@outlook.com>
2026-04-21 11:43:04 -0500

    Feature/app 001 crear aplicacion base (#13)
    * Creación de la aplicación básica
    
    * Estructura de proyecto actualizada y creado proyecto de pruebas unitarias
    
    * Observaciones corregidas

---
+++ app/source/HA.TFG.AppFinanzas.App/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using HA.TFG.AppFinanzas.App.Core.ViewModels;
+
+namespace HA.TFG.AppFinanzas.App
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new WelcomeViewModel();
+        }
+    }
+}
---
+++ app/source/HA.TFG.AppFinanzas.App/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace HA.TFG.AppFinanzas.App
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
